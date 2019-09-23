b1defea3eb1e1c873078bc9e71925c1c7ef13bf7
Paul Prestidge <paul@paulandemma.co.nz>
2019-09-23 19:35:14 +1200

    Add Station Core images and write a bit more in README.md

--- src/components/Base/Base.jsx
+++ src/components/Base/Base.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Upgrade from '../Upgrade';
 import './Base.scss';
+import * as bases from '../../assets/images/bases/*.png';
 
 const BASE_NAME = 'Station Core';
 
@@ -11,7 +12,12 @@ function Base({ friendly = false, owner }) {
 
       <div className="base__main">
         <div className="base__name">{BASE_NAME}</div>
-        <div className="base__image" />
+        <div className="base__image">
+          <img
+            src={bases[friendly ? 'station-core-1' : 'station-core-2']}
+            alt="Station Core"
+          />
+        </div>
         <div className="base__name base__name--reverse">{owner.name}</div>
       </div>
     </div>
