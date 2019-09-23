ed3027d15c83d334661607083e4fa2a5ad59b9d1
Paul Prestidge <paul@paulandemma.co.nz>
2019-09-23 19:11:14 +1200

    Make the report script a bit more robust

--- scripts/report.js
+++ scripts/report.js
@@ -2,62 +2,73 @@
 /* eslint-disable no-console */
 const fs = require('fs');
 
+const rulesets = ['2.4', '2.4.1'];
 const games = JSON.parse(fs.readFileSync('database.json'));
 const nonTestGames = games.filter(g => !g.players.map(p => p.name).join().match(/paul|test/i));
-const nonAiGames = nonTestGames.filter(g => !g.players.find(p => p.aiController));
-const currentVersionGames = nonAiGames.filter(g => g.ruleset === '2.4');
-const finishedGames = currentVersionGames.filter(g => g.gameState === 'finished');
+const finishedGames = nonTestGames.filter(g => g.gameState === 'finished');
+const currentVersionGames = finishedGames.filter(g => rulesets.includes(g.ruleset));
+const aiGames = currentVersionGames.filter(g => g.players.find(p => p.aiController));
+const humanGames = currentVersionGames.filter(g => !g.players.find(p => p.aiController));
 
 console.log(`Total records ingested: ${games.length}`);
-console.log(`Total finished games for current ruleset: ${finishedGames.length}`);
-console.log();
+console.log(`Including games with rulesets: ${JSON.stringify(rulesets)}`);
 
-console.log('How often does the player who goes first win?');
-console.log(finishedGames.reduce((memo, game) => {
-  const winner = game.activePlayer;
-  const firstPlayer = game.players[0].playerId;
+function statsForSegment(segment, segmentName) {
+  console.log(`=== ${segmentName} ===`);
 
-  return {
-    playFirst: memo.playFirst + (winner === firstPlayer ? 1 : 0),
-    playSecond: memo.playSecond + (winner === firstPlayer ? 0 : 1),
-  };
-}, { playFirst: 0, playSecond: 0 }));
-console.log();
+  console.log();
+  console.log(`Total finished games for current ruleset: ${segment.length}`);
 
-console.log('Game count by player name');
-console.log(finishedGames.flatMap(g => g.players).reduce((memo, player) => {
-  return {
-    ...memo,
-    [player.name]: (memo[player.name] || 0) + 1,
-  };
-}, {}));
-console.log();
+  console.log('How often does the player who goes first win?');
+  console.log(segment.reduce((memo, game) => {
+    const winner = game.activePlayer;
+    const firstPlayer = game.players[0].playerId;
 
-const allWinners = finishedGames.map(g => (
-  { ...g.players.find(p => p.playerId === g.activePlayer), log: g.log }
-));
+    return {
+      playFirst: memo.playFirst + (winner === firstPlayer ? 1 : 0),
+      playSecond: memo.playSecond + (winner === firstPlayer ? 0 : 1),
+    };
+  }, { playFirst: 0, playSecond: 0 }));
+  console.log();
 
-const cardsPlayedByWinners = allWinners.map(p => {
-  const playLogs = p.log.filter(l => l.action.type === 'play' && l.playerId === p.playerId);
-  return playLogs.map(l => [l.action.cardName, l.action.mode]);
-});
+  console.log('Game count by player name');
+  console.log(segment.flatMap(g => g.players).reduce((memo, player) => {
+    return {
+      ...memo,
+      [player.name]: (memo[player.name] || 0) + 1,
+    };
+  }, {}));
+  console.log();
 
-const winnerCardPlayFrequencies = cardsPlayedByWinners.flat().reduce((memo, combo) => {
-  return {
-    ...memo,
-    [combo]: (memo[combo] || 0) + 1,
-  };
-}, {});
+  const allWinners = segment.map(g => (
+    { ...g.players.find(p => p.playerId === g.activePlayer), log: g.log }
+  ));
 
-console.log('Average card plays by game for winners only');
-console.table(Object.entries(winnerCardPlayFrequencies).sort((a, b) => {
-  return a[1] - b[1];
-}).map(([ship, num]) => {
-  const [cardName, mode] = ship.split(',');
-  return {
-    cardName,
-    mode,
-    averagePlays: (num / finishedGames.length).toFixed(2),
-  };
-}));
-console.log();
+  const cardsPlayedByWinners = allWinners.map(p => {
+    const playLogs = p.log.filter(l => l.action.type === 'play' && l.playerId === p.playerId);
+    return playLogs.map(l => [l.action.cardName, l.action.mode]);
+  });
+
+  const winnerCardPlayFrequencies = cardsPlayedByWinners.flat().reduce((memo, combo) => {
+    return {
+      ...memo,
+      [combo]: (memo[combo] || 0) + 1,
+    };
+  }, {});
+
+  console.log('Average card plays by game for winners only');
+  console.table(Object.entries(winnerCardPlayFrequencies).sort((a, b) => {
+    return a[1] - b[1];
+  }).map(([ship, num]) => {
+    const [cardName, mode] = ship.split(',');
+    return {
+      cardName,
+      mode,
+      averagePlays: (num / segment.length).toFixed(2),
+    };
+  }));
+  console.log();
+}
+
+statsForSegment(aiGames, 'AI Games');
+statsForSegment(humanGames, 'Two-player Games');
