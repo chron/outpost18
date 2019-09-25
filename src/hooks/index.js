44e52e161e14a1a6477cba3cbdc4cb7673c27488
Paul Prestidge <paul@paulandemma.co.nz>
2019-09-25 20:15:42 +1200

    Try to solve the ios address bar issue by calculating window height in JS

--- src/hooks/index.js
+++ src/hooks/index.js
@@ -2,5 +2,6 @@ import useLocalStorage from './useLocalStorage';
 import useWebsocket from './useWebsocket';
 import useInterval from './useInterval';
 import useReplay from './useReplay';
+import useWindowSize from './useWindowSize';
 
-export { useLocalStorage, useWebsocket, useInterval, useReplay };
+export { useLocalStorage, useWebsocket, useInterval, useReplay, useWindowSize };
