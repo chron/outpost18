15075ca9592d35a2da292a527009b4dc1748c77f
Paul Prestidge <paul.prestidge@flickelectric.co.nz>
2019-10-05 10:23:20 +1300

    Console.log the whole environment for troubleshooting purposes :bug::gun:

--- src/lib/discordWebhooks.js
+++ src/lib/discordWebhooks.js
@@ -1,9 +1,10 @@
 import fetch from 'node-fetch';
 
-const { URL, DISCORD_WEBHOOK_URL } = process.env;
+const { CONTEXT, URL, DISCORD_WEBHOOK_URL, DISCORD_WEBHOOK_URL_DEV } = process.env;
+const url = CONTEXT === 'production' ? DISCORD_WEBHOOK_URL : DISCORD_WEBHOOK_URL_DEV;
 
 export async function reportFinishedGame(gameId, state) {
-  if (!DISCORD_WEBHOOK_URL) { return; }
+  if (!url) { return; }
   if (!state.gameState === 'finished') { return; }
 
   const winner = state.players.find(p => p.playerId === state.winner);
@@ -14,7 +15,7 @@ Replay link: ${URL}/replay/${gameId}`;
 
   // Because `wait` defaults to `false` we don't wait for the message to
   // actually post before returning.
-  await fetch(DISCORD_WEBHOOK_URL, {
+  await fetch(url, {
     method: 'POST',
     headers: { 'Content-Type': 'application/json' },
     body: JSON.stringify({ content }),
