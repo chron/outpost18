766cb5be7d4149f7f6f2f92caa16f25a62236bc4
Paul Prestidge <paul.prestidge@flickelectric.co.nz>
2019-10-06 13:40:57 +1300

    Temporarily comment out the player creation stuff until we can debug it properly

--- src/functions/identity-signup.js
+++ src/functions/identity-signup.js
@@ -1,22 +1,22 @@
-import { createPlayer as createPlayerData } from '../logic/playerManagement';
-import { createPlayer } from '../lib/database';
+// import { createPlayer as createPlayerData } from '../logic/playerManagement';
+// import { createPlayer } from '../lib/database';
 import { initializeErrorHandling, errorWrapper, reportError } from '../lib/errorHandling';
 
 async function handler(apiEvent, context) {
-  const { event, user } = JSON.parse(apiEvent.body);
+  // const { event, user } = JSON.parse(apiEvent.body);
 
-  const playerData = createPlayerData(user.sub, user.user_metadata.name, user.email);
+  // const playerData = createPlayerData(user.sub, user.user_metadata.name, user.email);
 
-  reportError(JSON.stringify({
-    event,
-    playerData,
-    apiEvent,
-    context,
-  }));
+  // reportError(JSON.stringify({
+  //   event,
+  //   playerData,
+  //   apiEvent,
+  //   context,
+  // }));
 
-  const newRef = await createPlayer(playerData);
+  // const newRef = await createPlayer(playerData);
 
-  reportError(JSON.stringify({ newRef }));
+  // reportError(JSON.stringify({ newRef }));
 
   return {
     statusCode: 204,
