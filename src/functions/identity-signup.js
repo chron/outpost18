8cd2f99de9c9d31d8101095ebf1624d86e09ba02
Paul Prestidge <paul.prestidge@flickelectric.co.nz>
2019-10-06 20:20:25 +1300

    Hmm

--- src/functions/identity-signup.js
+++ src/functions/identity-signup.js
@@ -1,22 +1,22 @@
-// import { createPlayer as createPlayerData } from '../logic/playerManagement';
-// import { createPlayer } from '../lib/database';
-//import { initializeErrorHandling, errorWrapper, reportError } from '../lib/errorHandling';
+import { createPlayer as createPlayerData } from '../logic/playerManagement';
+import { createPlayer } from '../lib/database';
+import { initializeErrorHandling, errorWrapper, reportError } from '../lib/errorHandling';
 
 async function handler(apiEvent, context) {
-  // const { event, user } = JSON.parse(apiEvent.body);
+  const { event, user } = JSON.parse(apiEvent.body);
 
-  // const playerData = createPlayerData(user.sub, user.user_metadata.name, user.email);
+  const playerData = createPlayerData(user.sub, user.user_metadata.name, user.email);
 
-  // reportError(JSON.stringify({
-  //   event,
-  //   playerData,
-  //   apiEvent,
-  //   context,
-  // }));
+  reportError(JSON.stringify({
+    event,
+    playerData,
+    apiEvent,
+    context,
+  }));
 
-  // const newRef = await createPlayer(playerData);
+  const newRef = await createPlayer(playerData);
 
-  // reportError(JSON.stringify({ newRef }));
+  reportError(JSON.stringify({ newRef }));
 
   return {
     statusCode: 200,
@@ -24,5 +24,5 @@ async function handler(apiEvent, context) {
   };
 }
 
-//initializeErrorHandling();
-exports.handler = handler; //errorWrapper(handler);
+initializeErrorHandling();
+exports.handler = errorWrapper(handler);
