1ed8c07510f16940d76d356b4b3b69a9b69d029f
Paul Prestidge <paul.prestidge@flickelectric.co.nz>
2019-10-07 08:50:14 +1300

    Track games by game type individually

--- src/logic/playerManagement.js
+++ src/logic/playerManagement.js
@@ -3,21 +3,32 @@ export function createPlayer(playerId, name, email) {
     playerId,
     name,
     email,
+    createdAt: new Date().toISOString(),
     games: {
+      wins: 0,
+      losses: 0,
+      elo: 1000,
+    },
+    privateGames: {
+      wins: 0,
+      losses: 0,
+    },
+    aiGames: {
       wins: 0,
       losses: 0,
     },
   };
 }
 
-export function recordGameResult(playerState, won) {
-  const { games, ...rest } = playerState;
+export function recordGameResult(playerState, gameType, won) {
+  const { [gameType]: oldGames, ...rest } = playerState;
 
   return {
     ...rest,
-    games: {
-      wins: games.wins + (won ? 1 : 0),
-      losses: games.losses + (won ? 0 : 1),
+    [gameType]: {
+      wins: oldGames.wins + (won ? 1 : 0),
+      losses: oldGames.losses + (won ? 0 : 1),
     },
+    lastUpdatedAt: new Date().toISOString(),
   };
 }
